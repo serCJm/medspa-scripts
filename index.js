03c6a7035ba19a162ec298238d6fe0c4fdf9a241
cjmoro <scjmoro@gmail.com>
2020-11-01 22:36:11 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ExcelJS = require("exceljs");
+(async () => {
+    const fileName = process.argv[2];
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(fileName);
+    const worksheet = workbook.getWorksheet("Export");
+    deleteNoShows(worksheet);
+    await workbook.xlsx.writeFile(fileName);
+})();
+function deleteNoShows(worksheet) {
+    worksheet.eachRow(function (row, rowNumber) {
+        const item = row.getCell(6);
+        const status = row.getCell(12);
+        if (typeof item.value === "string" &&
+            item.value.includes("No Show") &&
+            status.value === "unpaid")
+            worksheet.spliceRows(rowNumber, 1);
+    });
+}
